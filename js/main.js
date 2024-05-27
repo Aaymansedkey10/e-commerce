763912a14c08b24711c0bdce35d075b5230ff96c
Aymansedkey10 <am3423831@gmail.com>
2024-05-28 02:03:14 +0300

    update add to cart

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ let saleContent = document.querySelector(".sale-content");
 let blogContent = document.querySelector(".blog-content");
 
 // select items to event
-let btnClose = document.querySelector(".close");
 let btnSearch = document.querySelector(".btn-search");
 let cartIcon = document.querySelector(".cart");
 let bestSell = document.querySelector(".best-sell");
@@ -22,34 +21,6 @@ let womanCollection = document.querySelector(".woman");
 let kidsCollection = document.querySelector(".kids");
 let accCollection = document.querySelector(".accessories");
 let essenCollection = document.querySelector(".essential");
-
-// navBar.scrollY(200){
-
-// }
-// window.addEventListener("scroll" , ()=>{
-//     if (window.scrollY = 200) { // Change 200 to the distance you want
-//         // Add fixed class to navbar if scrolled past certain distance
-//         navBarar.classList.add('position-fixed');
-//       } else {
-//         // Remove fixed class from navbar if not scrolled past certain distance
-//         navBarar.classList.remove('position-fixed');
-//       }
-// })
-
-btnSearch.addEventListener("click" , async ()=>{
-
-    let response = await fetch (api);
-    let data = await response.json();
-    data.forEach(search => {
-        if (searchInput.value =="") {
-            console.log(false);
-        } else if(search.title.includes(searchInput.value)) {
-            
-        }
-    });    
-})
-
-
 // drop down selling
 bestSell.addEventListener("mouseenter", async () => {
   const response = await fetch(api);
@@ -74,18 +45,12 @@ bestSell.addEventListener("mouseleave", () => {
   bestItems.innerHTML = " ";
 });
 
-// show cart
-cartIcon.addEventListener("click", () => {
-  cartList.classList.add("show-cart");
-});
-
 // Man Collection
-async function getData() {
+async function manData() {
   const response = await fetch(api);
   const data = await response.json();
   const manProduct = data.filter((man) => man.category === "men's clothing");
   manProduct.forEach((element) => {
-    // console.log(element);
     contentTabOne.innerHTML += `<div class="col-6 col-lg-3">
                 <div class="card border-0 bg-transparent">
                     <div class="img-content position-relative">
@@ -107,7 +72,7 @@ async function getData() {
             </div>`;
   });
 }
-getData();
+manData();
 
 //  woman collection
 womanCollection.addEventListener("click", async () => {
@@ -137,6 +102,7 @@ womanCollection.addEventListener("click", async () => {
                 </div>
             </div>`;
   });
+  womanCollection.classList.add("disabled");
 });
 
 // kids Collection
@@ -165,42 +131,14 @@ kidsCollection.addEventListener("click", async () => {
                 </div>
             </div>`;
   });
+  kidsCollection.classList.add("disabled");
 });
-
-// accessories Collection
-accCollection.addEventListener("click", async () => {
-  const response = await fetch(api);
-  const data = await response.json();
-  let kids = data.filter((element) => element.category === "accessories");
-  kids.forEach((element) => {
-    contentTabFour.innerHTML += `<div class="col-6 col-lg-3">
-                <div class="card border-0 bg-transparent">
-                    <div class="img-content position-relative">
-                    <img src="${element.image}" class="card-img-top product-image" alt="Fissure in Sandstone"/>
-                    <div class="d-flex align-items-center justify-content-between w-100 bg-white position-absolute bottom-0 animate__animated animate__fadeInUp d-none container-cart">
-                            <button class="btn text-dark text-capitalize btn-card" onclick="addCard(${element.id})"> add to cart </button>
-                            <div class="cart-icon">
-                                <button class="btn"><i class="fa-solid fa-eye"></i> </button>
-                                <button class="btn" data-tooltip="hi" ><i class="fa-regular fa-heart"></i> </button>
-                                <button class="btn"><i class="fa-solid fa-chart-simple"></i> </button>
-                            </div>
-                        </div>
-                    </div> 
-                    <div class="card-body bg-transparent text-center">
-                        <h6 class="product-title">${element.title}</h6>
-                        <p class="product-price">${element.price} $</p>
-                    </div>
-                </div>
-            </div>`;
-  });
-});
-
 // essential Collection
 essenCollection.addEventListener("click", async () => {
   const response = await fetch(api);
   const data = await response.json();
-  let kids = data.filter((element) => element.category === "essential");
-  kids.forEach((element) => {
+  let essential = data.filter((element) => element.category === "essential");
+  essential.forEach((element) => {
     contentTabFive.innerHTML += `<div class="col-6 col-lg-3">
                 <div class="card border-0 bg-transparent">
                     <div class="img-content position-relative">
@@ -221,6 +159,7 @@ essenCollection.addEventListener("click", async () => {
                 </div>
             </div>`;
   });
+  essenCollection.classList.add("disabled");
 });
 
 // sales
@@ -253,7 +192,7 @@ async function blogData() {
   const data = await response.json();
   const blog = data.filter((element) => element.category === "blog");
   blog.forEach((element) => {
-    blogContent.innerHTML += `          <div class="col-12 col-lg-4">
+    blogContent.innerHTML += `          <div class="col-12 col-lg-4 mb-3 mb-lg-0">
         <div class="d-flex flex-column text-center content-blog pb-4">
           <img src="${element.image}" class="card-img-top mb-4" alt="image blog" srcset="">
           <p class="text-muted mb-1">${element.date}</p>
@@ -265,41 +204,112 @@ async function blogData() {
 }
 blogData();
 
-
-async function addCard(id) {
-  // console.log(id);
-  const response =await fetch(api);
-  const data = await response.json();
-  data.forEach(element => {
-     if (element.id == id) {
-      let cardEmpty = document.querySelector(".empty");
-      cardEmpty.style.display = "none"
-      cartList.innerHTML += `
-                              <div class="d-flex align-items-center justify-content-between mb-4 items-cart">
-                              <img src="${element.image}" class="rounded" alt="">
-                              <p class="price m-0 p-0"> ${element.price} $ </p>
-                              <p class="title m-0 p-0">${element.title}  </p>
-                              <div class="d-flex align-items-center justify-content-between">
-                                <button class=" p-2 rounded text-white border border-white bg-black"> + </button>
-                                <p class="count p-2 rounded text-white border border-white mx-3 my-0 p-0"> 0 </p>
-                                <button class=" p-2 rounded text-white border border-white bg-black"> - </button>
-                              </div>
-                              <button class=" p-2 rounded text-white border border-white bg-black remove"> × </button>
-                            </div>
-      `
-    }
-  });
-}
-btnClose.addEventListener("click" , ()=>{
+// open Cart
+cartIcon.addEventListener("click", () => {
+  cartList.classList.add("show-cart");
+});
+// close Cart 
+function closeCart(){
   cartList.classList.remove("show-cart");
-  // console.log("test");
-})
-let removeItem = document.querySelectorAll(".remove"); 
-console.log(removeItem);
-removeItem.addEventListener("click" , ()=>{
-  let items = document.querySelector(".item-cart")
-  removeItem.forEach(element => {
-    items.remove();
-  });
- 
-})
+  
+}
+// Add To Cart 
+async function addCard(id) {
+  const response = await fetch(api);
+  const data = await response.json();
+  if (!isItemInCart(id)) {
+    data.forEach(element => {
+      if (element.id == id) {
+        let cardEmpty = document.querySelector(".empty");
+        cardEmpty.style.display = "none";
+        cartList.innerHTML += createCard(element);
+      }
+    });
+  } else {
+    alert('Product added to cart');
+  }
+}
+function isItemInCart(id) {
+  let itemsInCart = document.querySelectorAll(".items-cart");
+  for (let item of itemsInCart) {
+    if (item.getAttribute('data-id') == id) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// cout of products  
+let proCount = 0
+let NumberCount = document.querySelector(".num-product");
+function countProductAdd() {
+  proCount ++ ;
+  NumberCount.innerHTML= proCount ;
+}
+
+function createCard(element) {
+  let count = 1;
+  countProductAdd();
+  return `
+    <div class="d-flex flex-column flex-lg-row flex-wrap align-items-center justify-content-between my-3 w-100 items-cart" data-id="${element.id}">
+      <img src="${element.image}" class="rounded" alt="">
+      <div class="d-flex align-items-center justify-content-between w-25 my-2 my-lg-0">
+        <p class="m-0 p-0"><span class="price"> ${element.price} </span> $ </p>
+        <p class="title w-25 m-0 p-0">${element.title}  </p>
+      </div>  
+      <div class="d-flex align-items-center justify-content-between mb-2 w-25 mb-lg-0">
+        <button class="btn btn-outline-primary p-2 rounded " onclick="increment(this)"> + </button>
+        <p class="count p-2 rounded text-white border border-white mx-2 my-0 p-0 Quantity">${count}</p>
+        <button class="p-2 btn btn-outline-success" onclick="decrement(this)"> - </button>
+        </div>
+        <button class="btn btn-outline-danger p-2 rounded remove" onclick="removeItem(${element.id})"> <i class="fa-solid fa-trash-can"></i> </button>
+    </div>
+  `;
+}
+
+function increment(button) {
+  let countElement = button.nextElementSibling;
+  let count = parseInt(countElement.textContent);
+  count++;
+  countElement.textContent = count;
+}
+
+function decrement(button) {
+  let countElement = button.previousElementSibling;
+  let count = parseInt(countElement.textContent);
+  if (count > 0) {
+    count--;
+  }
+  countElement.textContent = count;
+  if (count == 0) {
+    remove();
+  }
+}
+// remove item from cart
+function remove() {
+  let itemCart = document.querySelector(".items-cart");
+  itemCart.remove();
+}
+function removeItem(id){
+  remove();
+  countProductRemove();
+} 
+function countProductRemove() {
+  proCount -- ;
+  NumberCount.innerHTML = proCount ;
+}
+
+// checkOut 
+function checkOut() {
+  let itemsInCart = document.querySelectorAll(".items-cart");
+  let totalPrice = document.querySelector(".total-price");
+  let total = 0 ;
+  itemsInCart.forEach(items => {
+   let proPrice = items.querySelector(".price");
+  let proQuantity = items.querySelector(".Quantity");
+   let priceNumber = Number(proPrice.textContent);
+   let quantityNumber = Number(proQuantity.textContent);
+   total+= priceNumber * quantityNumber ;
+   totalPrice.innerHTML = total ;
+  }) 
+}
